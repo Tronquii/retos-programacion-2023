3b512113444b0c1813ca166c4bc8560d93dd91de
FranEscudero <103906252+Tronquii@users.noreply.github.com>
2023-01-13 04:16:35 +0100

    Add files via upload

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c#/FranEscudero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int x=0; x<=100;x++) 
+            {
+
+                if (x % 3 == 0 && x % 5 == 0 && x !=0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (x % 3 == 0 && x != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (x % 5 == 0 && x != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(x);
+                }
+                          
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
